cb93c824212f9cee4f8b50fe8c88bfa3b3903149
linkong <linkong0604@163.com>
2026-06-04 19:38:49 +0800

    change icon

--- src/config.ts
+++ src/config.ts
@@ -31,11 +31,11 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		    {
+			   src: "https://www.flaticon.com/free-icons/alphabet",    // Path of the favicon, relative to the /public directory
 		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+			},
 	],
 };
 
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://KuchikiRei.github.io", // Internal links should not include the base path, as it is automatically added
+			url: "https://kuchikirei.github.io", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -72,7 +72,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/kuchikirei",
 		},
 	],
 };
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,9 +1,13 @@
 # About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
+<!-- This is the site for [Fuwari](https://github.com/saicaca/fuwari). -->
+Welcome to [LinKong](https://github.com/kuchikirei)'s Blog 
 
-::github{repo="saicaca/fuwari"}
+::github{repo="kuchikirei/my_astro_site"}
 
-> ### Sources of images used in this site
-> - [Unsplash](https://unsplash.com/)
-> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
-> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+> ### My Friends
+> - [r1ce](https://https://r1ce.cn/)
+
+<!-- > ### Sources of images used in this site -->
+<!-- > - [Unsplash](https://unsplash.com/) -->
+<!-- > - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965) -->
+<!-- > - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty) -->
